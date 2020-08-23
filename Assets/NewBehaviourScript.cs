330c0ae9b3726874d5a5ecb70a7133475fcb7832
ArifEmreBahar <69960040+ArifEmreBahar@users.noreply.github.com>
2020-08-23 23:28:53 +0300

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 10f;
+    public float rotati = 1f;
+
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
