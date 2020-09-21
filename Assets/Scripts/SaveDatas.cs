050ed3005505935ba21930df088c8dbf13ba356c
ArifEmreBahar <69960040+ArifEmreBahar@users.noreply.github.com>
2020-09-21 19:32:12 +0300

    CheckPoint ve Save
    Check Point eklendi.
    Save Butonu eklendi.
    
    Eğer ölürse son CP de başlıyor.
    Pouse Menü den save alınırsa. Son CP den oyun başlıyor.
    B oyunu starta döndürüyor. Menüye çıkıp girmek gerekli.
    U karakteri öldürüyor.

---
+++ Assets/Scripts/SaveDatas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDatas : MonoBehaviour
+{
+    public Vector3 playerSpawnPoint;
+
+    public void GetSavedData()
+    {
+        playerSpawnPoint.x = PlayerPrefs.GetFloat("LastPositionX");
+        playerSpawnPoint.y = PlayerPrefs.GetFloat("LastPositionY");
+        playerSpawnPoint.z = PlayerPrefs.GetFloat("LastPositionZ");
+    }
+
+    public void Save()
+    {
+        playerSpawnPoint = GameObject.Find("White").GetComponent<SpawnPlayer>().lastPosition;
+        PlayerPrefs.SetFloat("LastPositionX", playerSpawnPoint.x);
+        PlayerPrefs.SetFloat("LastPositionY", playerSpawnPoint.y);
+        PlayerPrefs.SetFloat("LastPositionZ", playerSpawnPoint.z);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            playerSpawnPoint = new Vector3(-2f,68.3f,0f);
+            PlayerPrefs.SetFloat("LastPositionX", playerSpawnPoint.x);
+            PlayerPrefs.SetFloat("LastPositionY", playerSpawnPoint.y);
+            PlayerPrefs.SetFloat("LastPositionZ", playerSpawnPoint.z);
+
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public Vector3 lastPosition;
+    public SaveDatas saveData;
+
+
+    void OnEnable()
+    {
+            saveData.GetSavedData();
+            lastPosition = saveData.playerSpawnPoint;
+            spawnPlayer();
+    }
+
+    public void spawnPlayer() {
+        
+        transform.position = lastPosition;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U)) {
+            spawnPlayer();
+        }
+    }
+}
